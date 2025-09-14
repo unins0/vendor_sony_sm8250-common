bdc62a9a419d602f428fdb8f19e936643eeb20d7
unins0 <unins0000@proton.me>
2025-09-14 10:12:01 +0300

    Revert "sm8250-common: proprietary: Nuke dup av libs"
    This reverts commit f2daac447e4c95433d03c91c04f6b8111b41d2a1.

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -1044,6 +1044,23 @@ PRODUCT_PACKAGES += \
     com.qualcomm.qti.ant@1.0 \
     fm_helium \
     libfm-hci \
+    libmmosal \
+    libmmparser_lite \
+    libmmrtpdecoder \
+    libmmrtpencoder \
+    libwfdclient \
+    libwfdcommonutils \
+    libwfdconfigutils \
+    libwfddisplayconfig \
+    libwfdmminterface \
+    libwfdmmsink \
+    libwfdrtsp \
+    libwfdsinksm \
+    libwfduibcinterface \
+    libwfduibcsink \
+    libwfduibcsinkinterface \
+    libwfduibcsrc \
+    libwfduibcsrcinterface \
     vendor.qti.hardware.audiohalext@1.0 \
     vendor.qti.hardware.fm@1.0 \
     com.qualcomm.qti.dpm.api@1.0 \
